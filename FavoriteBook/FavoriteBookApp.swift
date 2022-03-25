eec3370bfa577f44aca7130a0a75e64fa9ff284b
Birkan <birkanpusa@icloud.com>
2022-03-25 11:19:47 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Birkan Pusa on 25.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
